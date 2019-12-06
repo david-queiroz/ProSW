08bcfd29069239ee87ffd24a192641b2eaafb035
David Wesley <david.wesley@tevec.com.br>
2019-12-05 22:24:10 -0300

    rotas de desenvolvimento

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let app = require('./config/custom-express')();
-let consign = require('consign')
+
 app.listen(8080, function(){
     console.log('Servidor rodando na porta 8080.');
     
--- rotas/executivo.js
+++ rotas/executivo.js
@@ -1,7 +1,45 @@
-module.exports = function(app){
-    app.get('/executivo', function(req, res){
-        console.log('Recebida requisicao de teste na porta 8080.')
-        res.send('ok.');
-});
+module.exports = app => {
+//acoes rota /executivo
 
-}
+    app.post('/executivo', (req, res) => {
+        console.log('Recebida requisicao de teste na porta 8080.')
+        res.status(201).send('ok.');
+    });
+
+    app.put('/executivo', (req, res) => {
+        console.log('Recebida requisicao de teste na porta 8080.')
+        res.status(203).send('ok.');
+    });
+
+    app.get('/executivo', (req, res) => {
+        console.log('Recebida requisicao execultivo.')
+        res.send('OK');
+    });
+
+    app.delete('/executivo', (req, res) => {
+        console.log('Recebida requisicao execultivo.')
+        res.send('ok.');
+    });
+//acoes rota /desenvolvimento
+
+    app.post('/desenvolvimento', (req, res) => {
+        console.log('Recebida requisicao de teste na porta 8080.')
+        res.status(201).send('ok.');
+    });
+    
+    app.put('/desenvolvimento', (req, res) => {
+        console.log('Recebida requisicao de teste na porta 8080.')
+        res.status(203).send('ok.');
+    });
+    
+    app.get('/desenvolvimento', (req, res) => {
+        console.log('Recebida requisicao execultivo.')
+        res.send('OK');
+    });
+    
+    app.delete('/desenvolvimento', (req, res) => {
+        console.log('Recebida requisicao execultivo.')
+        res.send('ok.');
+    });
+    
+    };
